dd42cf740205d89d47bbef7fd15e690e10f3934a
kayvank <kayvan@q2io.com>
2022-12-17 09:42:00 -0800

    haskell tab fix

--- conf/org-journal.el
+++ conf/org-journal.el
@@ -1,4 +1,6 @@
 ;;; conf/org-journal.el -*- lexical-binding: t; -*-
+;;;
+
 (use-package! org-capture
   :defer t
   :init
@@ -18,4 +20,19 @@
   )
   )
 )
+(use-package! websocket
+    :after org-roam)
+
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
   (setq diary-file "~/.shared/shared/emacs/diary")
